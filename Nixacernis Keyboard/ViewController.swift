a4e9a497044c4150cc7a1067605ec603883ceed2
MapleCCC <littlelittlemaple@gmail.com>
2019-03-07 00:10:01 +0800

    Initial Commit

---
+++ Nixacernis Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nixacernis Keyboard
+//
+//  Created by CHEN Yifeng on 7/3/19.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
